974cba03b1169c01004468a3af2e7d0fd2db698f
insanetim <47120724+insanetim@users.noreply.github.com>
2023-02-24 17:15:04 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,73 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[name].[ext]'
+module.exports = (env = {}) => {
+  const { mode = 'development' } = env
+  const isDev = mode === 'development'
+
+  const getStyleLoaders = () => [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+
+  return {
+    mode,
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: getStyleLoaders()
+        },
+        {
+          test: /\.(png|jp(e*)g|svg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'images/[name].[ext]'
+              }
             }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-      filename: './index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, './public')
+          ]
+        }
+      ]
     },
-    port: 8080,
-    historyApiFallback: true
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    extensions: ['*', '.js'],
-    alias: {
-      src: path.resolve(__dirname, 'src')
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+        filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css'
+      })
+    ],
+
+    devServer: {
+      open: true,
+      port: 8080,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, './public')
+      }
+    },
+
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      extensions: ['*', '.js'],
+      alias: {
+        src: path.resolve(__dirname, 'src')
+      }
+    },
+
+    devtool: isDev ? 'eval-source-map' : false,
+
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
     }
-  },
-  devtool: 'source-map'
+  }
 }
