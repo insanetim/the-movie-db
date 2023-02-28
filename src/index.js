3cf60278b5ae5d030ffaf7cb873aeb75b8722924
insanetim <47120724+insanetim@users.noreply.github.com>
2023-02-28 17:48:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import './assets/styles/app.scss'
 
 import App from './App'
 import store from './state/store'
+import './assets/styles/app.scss'
 
 const rootElement = document.getElementById('app')
 
