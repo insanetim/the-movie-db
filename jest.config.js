2b127973b97dfbc9711c8e49a24d080272930449
insanetim <47120724+insanetim@users.noreply.github.com>
2023-03-21 15:15:14 +0200

    jest config added

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  },
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/index.js'],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: ['node_modules'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/file_mock.js',
+    '\\.(s?css)$': 'identity-obj-proxy',
+    '^src/(.*)$': '<rootDir>/src/$1'
+  }
+}
