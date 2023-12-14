73dd8f0fa7ffe90c473be303ae0f21cccd278d51
insanetim <47120724+insanetim@users.noreply.github.com>
2023-12-14 16:44:05 +0200

    update snapshots

--- src/components/Lists/ListsList/component.tsx
+++ src/components/Lists/ListsList/component.tsx
@@ -6,22 +6,26 @@ import { ListsListProps } from './types'
 const ListsList: React.FC<ListsListProps> = ({ lists }) => {
   return (
     <Row gutter={[24, 16]}>
-      {lists.map(list => (
-        <Col
-          key={list.id}
-          lg={8}
-          md={8}
-          sm={12}
-          span={24}
-          xl={6}
-        >
-          <ListItem
-            description={list.description}
-            id={list.id}
-            name={list.name}
-          />
-        </Col>
-      ))}
+      {lists.map(list => {
+        const { description, id, name } = list
+
+        return (
+          <Col
+            key={id}
+            lg={8}
+            md={8}
+            sm={12}
+            span={24}
+            xl={6}
+          >
+            <ListItem
+              description={description}
+              id={id}
+              name={name}
+            />
+          </Col>
+        )
+      })}
     </Row>
   )
 }
--- src/components/Movies/MoviesList/component.tsx
+++ src/components/Movies/MoviesList/component.tsx
@@ -9,24 +9,28 @@ const MoviesList: React.FC<MoviesListProps> = ({
 }) => {
   return (
     <Row gutter={[24, 16]}>
-      {movies.map(movie => (
-        <Col
-          key={movie.id}
-          lg={8}
-          md={8}
-          sm={12}
-          span={24}
-          xl={6}
-        >
-          <MovieItem
-            handleMovieDelete={handleMovieDelete}
-            id={movie.id}
-            overview={movie.overview}
-            posterPath={movie.poster_path}
-            title={movie.title}
-          />
-        </Col>
-      ))}
+      {movies.map(movie => {
+        const { id, overview, poster_path, title } = movie
+
+        return (
+          <Col
+            key={id}
+            lg={8}
+            md={8}
+            sm={12}
+            span={24}
+            xl={6}
+          >
+            <MovieItem
+              handleMovieDelete={handleMovieDelete}
+              id={id}
+              overview={overview}
+              posterPath={poster_path}
+              title={title}
+            />
+          </Col>
+        )
+      })}
     </Row>
   )
 }
