3d172da4ef7694f0c3fe7a146ddd33d0981ecf2e
insanetim <47120724+insanetim@users.noreply.github.com>
2023-12-22 22:26:44 +0200

    update

--- src/__mocks__/mockNotification.ts
+++ src/__mocks__/mockNotification.ts
@@ -2,7 +2,7 @@ import { NOTIFICATION_DURATION, NOTIFICATION_TYPE } from 'src/constants/app'
 
 const mockNotification = {
   duration: NOTIFICATION_DURATION,
-  id: 'nanoid',
+  id: 'test/id',
   messageText: 'test/message',
   messageType: NOTIFICATION_TYPE.SUCCESS,
 }
