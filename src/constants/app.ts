9efa0f9cb7090b8e144c362e8d8ff69f6ffe5abd
insanetim <47120724+insanetim@users.noreply.github.com>
2024-02-23 16:48:32 +0200

    update constants

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,12 +1,17 @@
 const APP_NAME = 'The Movie DB'
 const API_URL = 'https://api.themoviedb.org/3'
 const ACCESS_TOKEN_AUTH = process.env.ACCESS_TOKEN_AUTH
-enum NOTIFICATION_TYPE {
+const enum NOTIFICATION_TYPE {
   ERROR = 'error',
   SUCCESS = 'success',
 }
 const NOTIFICATION_DURATION = 2.5
-const GENDERS = ['Not set / not specified', 'Female', 'Male', 'Non-binary']
+const GENDERS = [
+  'Not set / not specified',
+  'Female',
+  'Male',
+  'Non-binary',
+] as const
 
 export {
   ACCESS_TOKEN_AUTH,
