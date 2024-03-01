c847070aeb7cad712263b4c9b32111bd08a6c09a
insanetim <47120724+insanetim@users.noreply.github.com>
2024-03-01 11:20:25 +0200

    update personDetails tests

--- src/pages/PersonDetails/__tests__/hook.test.ts
+++ src/pages/PersonDetails/__tests__/hook.test.ts
@@ -1,5 +1,5 @@
-import { renderHook } from '@testing-library/react'
-import { useParams } from 'react-router-dom'
+import { act, renderHook } from '@testing-library/react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { mockPersonDetails } from 'src/__mocks__/mockPerson'
 import { dispatch } from 'src/__mocks__/react-redux'
 import * as personDetailsActions from 'src/store/personDetails/actions'
@@ -9,8 +9,11 @@ import useContainer from '../hook'
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
   useParams: jest.fn(() => ({ personSlug: '1234-john-doe' })),
 }))
+const navigate = jest.fn()
+jest.mocked(useNavigate).mockReturnValue(navigate)
 
 jest.mock('src/store/personDetails/selectors')
 
@@ -36,6 +39,16 @@ describe('PersonDetails useContainer hook', () => {
     const { result } = renderHook(useContainer)
 
     expect(result.current).toMatchSnapshot()
+  })
+
+  it('should check "handleGoToCast" method', () => {
+    const { result } = renderHook(useContainer)
+
+    act(() => {
+      result.current.handleGoToCredits()
+    })
+
+    expect(navigate).toHaveBeenCalledWith('credits')
   })
 
   it('should check "useEffect" method', () => {
