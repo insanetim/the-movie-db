de485c5cc561dada1ef196fd93e0a93d047054e7
insanetim <47120724+insanetim@users.noreply.github.com>
2024-11-01 15:39:44 +0200

    update utils

--- src/utils/helpers/favoriteMessage.ts
+++ src/utils/helpers/favoriteMessage.ts
@@ -1,4 +1,4 @@
 const favoriteMessage = (title: string, inFavorite: boolean) =>
-  `${title} ${inFavorite ? 'added to Favorite' : 'removed from Favorite'}`
+  `${title} ${inFavorite ? 'added to' : 'removed from'} Favorite`
 
 export default favoriteMessage
--- src/utils/helpers/watchlistMessage.ts
+++ src/utils/helpers/watchlistMessage.ts
@@ -1,4 +1,4 @@
 const watchlistMessage = (title: string, inWatchlist: boolean) =>
-  `${title} ${inWatchlist ? 'added to Watchlist' : 'removed from Watchlist'}`
+  `${title} ${inWatchlist ? 'added to' : 'removed from'} Watchlist`
 
 export default watchlistMessage
