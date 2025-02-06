64ac42e9ab4e45323b2de2c558ba85ae11914abb
insanetim <47120724+insanetim@users.noreply.github.com>
2025-02-06 18:49:46 +0200

    update jest setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,5 @@
 import '@testing-library/jest-dom'
 import 'src/utils/testHelpers/matchMedia'
+import { TextDecoder, TextEncoder } from 'node:util'
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder as never
