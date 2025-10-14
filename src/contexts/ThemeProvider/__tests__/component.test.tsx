7bab11f3b559e42c4761140f54cb2112dc0a06a5
insanetim <47120724+insanetim@users.noreply.github.com>
2025-10-14 18:27:19 +0300

    add ThemeProvider component tests

---
+++ src/contexts/ThemeProvider/__tests__/component.test.tsx
@@ -0,0 +1,22 @@
+import { renderWithWrapper } from 'src/utils/testHelpers/renderWithWrapper'
+
+import ThemeProvider from '../component'
+import { ThemeProviderHookReturn } from '../types'
+
+const mockedHook: ThemeProviderHookReturn = {
+  algorithm: jest.fn(),
+  isDark: false,
+}
+jest.mock('../hook', () => jest.fn(() => mockedHook))
+
+describe('ThemeProvider component', () => {
+  it('should match snapshot', () => {
+    const { asFragment } = renderWithWrapper(
+      <ThemeProvider>
+        <div data-testid='child'>content</div>
+      </ThemeProvider>
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
