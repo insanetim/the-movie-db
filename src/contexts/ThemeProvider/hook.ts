20f8ee6e2cba4fad452aba86b4c1d34fe74e0ba0
insanetim <47120724+insanetim@users.noreply.github.com>
2025-10-14 15:58:02 +0300

    update ThemeProvider

---
+++ src/contexts/ThemeProvider/hook.ts
@@ -0,0 +1,15 @@
+import { theme } from 'antd'
+import { themeSelector } from 'src/store/app/reducer'
+import { useAppSelector } from 'src/store/hooks'
+
+import { ThemeProviderHookReturn } from './types'
+
+const useContainer = (): ThemeProviderHookReturn => {
+  const { darkAlgorithm, defaultAlgorithm } = theme
+  const currentTheme = useAppSelector(themeSelector)
+  const isDark = currentTheme === 'dark'
+
+  return { algorithm: isDark ? darkAlgorithm : defaultAlgorithm, isDark }
+}
+
+export default useContainer
