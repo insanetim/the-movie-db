d93973a7d55be3f50113414ca003472c37ba274f
insanetim <47120724+insanetim@users.noreply.github.com>
2025-10-18 23:37:49 +0300

    update mocks

--- src/__mocks__/mockAccount.ts
+++ src/__mocks__/mockAccount.ts
@@ -1,15 +1,12 @@
 import { IAccount } from 'src/interfaces/account.interface'
 
 const mockAccount: IAccount = {
-  avatar: {
-    gravatar: {
-      hash: 'image',
-    },
-    tmdb: {
-      avatar_path: undefined,
-    },
-  },
+  avatar: { gravatar: { hash: 'image' }, tmdb: { avatar_path: null } },
   id: 1234,
+  include_adult: true,
+  iso_639_1: 'en',
+  iso_3166_1: 'US',
+  name: 'test/name',
   username: 'test/username',
 }
 
--- src/__mocks__/mockState.ts
+++ src/__mocks__/mockState.ts
@@ -1,7 +1,5 @@
 import { apiSlice } from 'src/store/api/apiSlice'
 import { RootState } from 'src/store/index'
-import { movieDetailsInitialState } from 'src/store/movieDetails/reducer'
-import { personDetailsInitialState } from 'src/store/personDetails/reducer'
 
 const mockState: RootState = {
   [apiSlice.reducerPath]: {
@@ -33,34 +31,7 @@ const mockState: RootState = {
       version: -1,
     },
     account: null,
-    isAuthenticated: false,
-  },
-  createdLists: {
-    data: null,
-    error: null,
-    loading: true,
-  },
-  dashboard: {
-    data: null,
-    error: null,
-    loading: true,
-  },
-  favorite: {
-    data: null,
-    error: null,
-    loading: true,
-  },
-  listDetails: {
-    data: null,
-    error: null,
-    loading: true,
-  },
-  movieDetails: movieDetailsInitialState,
-  personDetails: personDetailsInitialState,
-  watchlist: {
-    data: null,
-    error: null,
-    loading: true,
+    sessionId: null,
   },
 }
 
