e464e848ce3556150b24269972bce46f1951884c
insanetim <47120724+insanetim@users.noreply.github.com>
2025-10-20 19:09:25 +0300

    update hooks tests

--- src/hooks/__tests__/useScrollToTop.test.ts
+++ src/hooks/__tests__/useScrollToTop.test.ts
@@ -1,3 +1,7 @@
+import type { Location } from 'react-router-dom'
+
+import { act } from '@testing-library/react'
+import { useLocation } from 'react-router-dom'
 import { renderHookWithWrapper } from 'src/utils/testHelpers/renderWithWrapper'
 
 import useScrollToTop from '../useScrollToTop'
@@ -6,17 +10,74 @@ jest.useFakeTimers()
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useLocation: jest.fn(() => ({ pathname: '/' })),
+  useLocation: jest.fn(),
 }))
 
-const spyScrollTo = jest.fn()
-Object.defineProperty(global.window, 'scrollTo', { value: spyScrollTo })
+const mockUseLocation = useLocation as jest.MockedFunction<typeof useLocation>
+const scrollToSpy: typeof window.scrollTo = jest.fn()
+
+Object.defineProperty(window, 'scrollTo', {
+  configurable: true,
+  value: scrollToSpy,
+})
+
+const createLocation = (overrides: Partial<Location> = {}): Location => ({
+  hash: '',
+  key: 'mock-key',
+  pathname: '/',
+  search: '',
+  state: null,
+  ...overrides,
+})
 
 describe('useScrollToTop', () => {
-  it('should call "scrollTo" method', () => {
-    renderHookWithWrapper(useScrollToTop)
-    jest.runAllTimers()
+  beforeEach(() => {
+    jest.clearAllMocks()
+    jest.clearAllTimers()
+  })
 
-    expect(spyScrollTo).toHaveBeenCalledWith(0, 0)
+  afterAll(() => {
+    jest.useRealTimers()
+  })
+
+  it('should call "scrollTo" method', () => {
+    let currentPathname = '/'
+
+    mockUseLocation.mockImplementation(() =>
+      createLocation({ pathname: currentPathname })
+    )
+
+    const { rerender } = renderHookWithWrapper(useScrollToTop)
+
+    act(() => {
+      jest.runAllTimers()
+    })
+
+    expect(scrollToSpy).toHaveBeenCalledWith(0, 0)
+    expect(scrollToSpy).toHaveBeenCalledTimes(1)
+
+    currentPathname = '/next'
+    rerender()
+
+    act(() => {
+      jest.runOnlyPendingTimers()
+    })
+
+    expect(scrollToSpy).toHaveBeenCalledTimes(2)
+  })
+
+  it('should clear timeout on cleanup', () => {
+    mockUseLocation.mockReturnValue(createLocation({ pathname: '/' }))
+
+    const clearTimeoutSpy = jest.spyOn(globalThis, 'clearTimeout')
+    const setTimeoutSpy = jest.spyOn(globalThis, 'setTimeout')
+
+    const { unmount } = renderHookWithWrapper(useScrollToTop)
+
+    expect(setTimeoutSpy).toHaveBeenCalledWith(expect.any(Function), 0)
+
+    unmount()
+
+    expect(clearTimeoutSpy).toHaveBeenCalledWith(expect.any(Number))
   })
 })
