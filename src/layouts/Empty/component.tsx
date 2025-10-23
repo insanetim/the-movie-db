c4ef4598bec375cde9e68c7582f74852ecb96161
insanetim <47120724+insanetim@users.noreply.github.com>
2025-10-23 11:14:06 +0300

    update styles

--- src/layouts/Empty/component.tsx
+++ src/layouts/Empty/component.tsx
@@ -3,7 +3,7 @@ import { Layout } from 'antd'
 import { EmptyLayoutProps } from './types'
 
 const EmptyLayout: React.FC<EmptyLayoutProps> = ({ children }) => (
-  <Layout className='center'>
+  <Layout className='full-height'>
     <Layout.Content className='auth-layout'>{children}</Layout.Content>
   </Layout>
 )
