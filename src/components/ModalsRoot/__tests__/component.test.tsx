e9b92cd70a24e8e2cb91129a3509171be9b0c4eb
insanetim <47120724+insanetim@users.noreply.github.com>
2025-10-24 17:51:03 +0300

    update ModalRoot component test

--- src/components/ModalsRoot/__tests__/component.test.tsx
+++ src/components/ModalsRoot/__tests__/component.test.tsx
@@ -1,8 +1,31 @@
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithWrapper } from 'src/utils/testHelpers/renderWithWrapper'
 
 import ModalsRoot from '../component'
 import useContainer from '../hook'
 import { ModalRootHookReturn } from '../types'
+
+const capturedModalProps: { current?: Record<string, unknown> } = {}
+
+jest.mock('../modalComponents', () => {
+  const actual = jest.requireActual('../modalComponents')
+
+  return {
+    ...actual,
+    MODAL_COMPONENTS: {
+      ...actual.MODAL_COMPONENTS,
+      CREATE_LIST: (props: Record<string, unknown>) => {
+        capturedModalProps.current = props
+
+        const ActualComponent = actual.MODAL_COMPONENTS.CREATE_LIST as (
+          props: Record<string, unknown>
+        ) => JSX.Element
+
+        return ActualComponent(props)
+      },
+    },
+  }
+})
 
 jest.mock('../hook')
 
@@ -13,6 +36,7 @@ const mockedUseContainer = useContainer as jest.MockedFunction<
 describe('ModalsRoot component', () => {
   beforeEach(() => {
     jest.clearAllMocks()
+    capturedModalProps.current = undefined
   })
 
   it('matches snapshot with active modal', () => {
@@ -46,5 +70,63 @@ describe('ModalsRoot component', () => {
     const { baseElement } = renderWithWrapper(<ModalsRoot />)
 
     expect(baseElement).toMatchSnapshot()
+  })
+
+  it('calls modal afterClose and removes modal when ModalComponent afterClose is triggered', () => {
+    const removeModal = jest.fn()
+    const closeModal = jest.fn()
+    const modalAfterClose = jest.fn()
+    const modal: ModalRootHookReturn['modals'][number] = {
+      modalId: 'modal-1',
+      modalProps: {
+        afterClose: modalAfterClose,
+      },
+      modalType: 'CREATE_LIST',
+    }
+
+    mockedUseContainer.mockReturnValue({
+      closeModal,
+      modals: [modal],
+      removeModal,
+    })
+
+    renderWithWrapper(<ModalsRoot />)
+
+    const props = capturedModalProps.current
+
+    expect(props).toBeDefined()
+
+    const afterClose = props?.afterClose as (() => void) | undefined
+
+    expect(afterClose).toBeDefined()
+
+    afterClose?.()
+
+    expect(modalAfterClose).toHaveBeenCalledTimes(1)
+    expect(removeModal).toHaveBeenCalledWith('modal-1')
+  })
+
+  it('calls closeModal when ModalComponent onCancel is triggered', async () => {
+    const removeModal = jest.fn()
+    const closeModal = jest.fn()
+    const modal: ModalRootHookReturn['modals'][number] = {
+      modalId: 'modal-2',
+      modalProps: {},
+      modalType: 'CREATE_LIST',
+    }
+
+    mockedUseContainer.mockReturnValue({
+      closeModal,
+      modals: [modal],
+      removeModal,
+    })
+
+    renderWithWrapper(<ModalsRoot />)
+
+    const closeButton = await screen.findByRole('button', { name: /close/i })
+
+    fireEvent.click(closeButton)
+
+    expect(closeModal).toHaveBeenCalledWith('modal-2')
   })
 })
