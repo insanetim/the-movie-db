688a43c71f591b386cd12f98746e0a1379eca6e2
insanetim <47120724+insanetim@users.noreply.github.com>
2025-10-25 11:26:09 +0300

    update api tests

--- src/api/imdb/__tests__/apiClient.test.ts
+++ src/api/imdb/__tests__/apiClient.test.ts
@@ -1,7 +1,10 @@
+import { IMDB_API_URL } from 'src/constants'
+
 import imdbClient from '../apiClient'
 
 describe('imdbClient', () => {
-  it('should match snapshot', () => {
-    expect(imdbClient).toMatchSnapshot()
+  it('should configure axios client with base URL and timeout', () => {
+    expect(imdbClient.defaults.baseURL).toBe(IMDB_API_URL)
+    expect(imdbClient.defaults.timeout).toBe(10000)
   })
 })
